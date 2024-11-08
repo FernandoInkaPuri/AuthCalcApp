7edad47f6c3b169dc849ba6be8ea6b0029456b93
Luiz Fernando <luizfegomez@gmail.com>
2024-11-08 06:01:50 -0300

    Add calculate route and it is working

--- node_auth_api/index.js
+++ node_auth_api/index.js
@@ -2,10 +2,39 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/calculate', (req, res) => {
+  const { value1, value2, operation } = req.body;
+
+  let result;
+
+  switch (operation) {
+    case 'sum':
+      result = value1 + value2;
+      break;
+    case 'subtract':
+      result = value1 - value2;
+      break;
+    case 'multiply':
+      result = value1 * value2;
+      break;
+    case 'divide':
+      if (value2 === 0) {
+        return res.status(400).send('Não é possível dividir por zero');
+      }
+      result = value1 / value2;
+      break;
+    default:
+      return res.status(400).send('Operação inválida');
+  }
+  res.send(`O resultado é ${result}`)
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Servidor inciado na porta ${port}`)
 })
